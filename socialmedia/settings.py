2aadced8cef176ff6f9b0fbaeab92f3906d2a623
4joejoe <shantanuchaubey1@gmail.com>
2024-05-31 04:42:31 +0530

    Update ALLOWED_HOSTS in settings.py

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-5b(cx5*8x1z1tod*!hs3zo4gij+xovnm!+hsd3suqjp_dhsf1r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["socialmedia-ajee.onrender.com"]
 
 # JWT Settings
 SIMPLE_JWT = {
