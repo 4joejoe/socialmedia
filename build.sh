5fee7e540126bf1e5f823642a30ad59288f53be9
4joejoe <shantanuchaubey1@gmail.com>
2024-05-31 05:09:02 +0530

    Add migrations for authapp in build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ pip install -r requirements.txt
 
 echo "Making migrations..."
 python3 manage.py makemigrations
+python3 manage.py makemigrations authapp
 
 echo "Applying migrations..."
 python3 manage.py migrate
